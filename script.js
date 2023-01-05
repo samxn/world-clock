8c0cec4a1fb080a2a04000fc2b97f305b1f35ca0
Samin <86338112+samxn@users.noreply.github.com>
2023-01-04 19:59:43 -0500

    Added js to allow for select to function

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
 function updateTime() {
   // city 1
   let city1Element = document.querySelector("#city1");
-  let city1DateElement = city1Element.querySelector(".date");
-  let city1TimeElement = city1Element.querySelector(".time");
+  if (city1Element) {
+    let city1DateElement = city1Element.querySelector(".date");
+    let city1TimeElement = city1Element.querySelector(".time");
 
-  let city1 = moment().tz("Europe/Paris");
+    let city1 = moment().tz("Europe/Paris");
 
-  city1DateElement.innerHTML = city1.format("MMMM Do YYYY");
-  city1TimeElement.innerHTML = city1.format("h:m [<small>]A[</small>]");
-}
-updateTime();
-setInterval(updateTime, 10000);
+    city1DateElement.innerHTML = city1.format("MMMM Do YYYY");
+    city1TimeElement.innerHTML = city1.format("h:m [<small>]A[</small>]");
+  }
+  // city 2
+  let city2Element = document.querySelector("#city2");
+  if (city2Element) {
+    let city2DateElement = city2Element.querySelector(".date");
+    let city2TimeElement = city2Element.querySelector(".time");
 
-function showTime(event) {
-  if (event.target.value.length > 0) {
-    if (event.target.value === "paris") {
-      let parisTime = moment()
-        .tz("Europe/Paris")
-        .format("dddd, MMMM D, YYYY h:m A");
-      alert(`It is ${parisTime} in Paris, France`);
-    }
-    if (event.target.value === "milan") {
-      let milanTime = moment().tz("CET").format("dddd, MMMM D, YYYY h:m A");
-      alert(`It is ${milanTime} in Milan, Italy`);
-    }
-    if (event.target.value === "atlanta") {
-      let atlantaTime = moment()
-        .tz("America/New_York")
-        .format("dddd, MMMM D, YYYY h:m A");
-      alert(`It is ${atlantaTime} in Atlanta, Georgia`);
-    }
+    let city2 = moment().tz("Australia/Queensland");
 
-    if (event.target.value === "orlando") {
-      let orlandoTime = moment()
-        .tz("America/New_York")
-        .format("dddd, MMMM D, YYYY h:m A");
-      alert(`It is ${orlandoTime} in Orlando, Florida`);
-    }
-
-    if (event.target.value === "unitedKingdom") {
-      let ukTime = moment().tz("Etc/GMT").format("dddd, MMMM D, YYYY h:m A");
-      alert(`It is ${ukTime} in the London, United Kingdom`);
-    }
+    city2DateElement.innerHTML = city2.format("MMMM Do YYYY");
+    city2TimeElement.innerHTML = city2.format("h:m [<small>]A[</small>]");
   }
 }
 
-let citySelect = document.querySelector("#clock");
-citySelect.addEventListener("change", showTime);
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+ <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("h:m ")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+        </div>
+
+`;
+}
+
+updateTime();
+setInterval(updateTime, 10000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
